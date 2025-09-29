3884d7e05232567ca5b03da5682162b83e8f3fed
QUIDA5V <dquinonezlib@gmail.com>
2025-09-29 12:52:01 -0500

    test

---
+++ .github/actions/state-file/entrypoint.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+file_path=$1     # Ruta al kustomization.yaml dentro del repo destino
+image_name=$2    # Nombre de la imagen
+image_tag=$3     # Tag nuevo de la imagen
+
+echo "➡️ Updating kustomization file: $file_path"
+echo "➡️ Setting image: $image_name:$image_tag"
+
+# Ir a la carpeta donde está el archivo
+cd "$(dirname "$file_path")"
+
+# Actualizar la imagen con kustomize
+kustomize edit set image "$image_name=$image_name:$image_tag"
+
+echo "✅ kustomization.yaml updated successfully"
